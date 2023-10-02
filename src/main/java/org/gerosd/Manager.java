dee245e18d7e6a85f63aa4b420d95886d6c7aa31
focks <focksikan@gmail.com>
2023-10-02 17:25:57 +0300

    Learning Logger

---
+++ src/main/java/org/gerosd/Manager.java
@@ -0,0 +1,10 @@
+package org.gerosd;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Manager {
+    private static final Logger logger = LogManager.getLogger(Manager.class);
+
+
+}
