b407948b882ab68ce3c98f739009163bec59b9b3
focks <focksikan@gmail.com>
2023-10-04 15:44:24 +0300

    Idk why it doesn`t work

--- src/main/java/org/gerosd/Manager.java
+++ src/main/java/org/gerosd/Manager.java
@@ -1,10 +1,14 @@
 package org.gerosd;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Manager {
-    private static final Logger logger = LogManager.getLogger(Manager.class);
+    private static final Logger logger = LoggerFactory.getLogger(Manager.class);
 
-
+    public static void main(String[] args) {
+        PropertyConfigurator.configure("log4j.xml");
+        logger.info("Hello World");
+    }
 }
